394a2a1c758c0d599e7fd21f85dca273ba41ea63
azncrusader <c.nachos12@gmail.com>
2024-08-28 20:27:28 +0000

    ayylmao

---
+++ testTalent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git clone https://github.com/Junove/TalentManagementBackend.git
+
+cd TalentManagementBackend/
+
+gradle -stop
+
+gradle wrapper
+
+gradle build
+
+./gradlew bootRun &
+
+cd ..
+
+git clone https://github.com/Junove/TalentManagementFrontend.git
+
+cd TalentManagementFrontend/
+
+npm install
+
+npm run start
+
